781af1cd9624892b41c86a9e5150015090f307c4
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-06 20:52:21 +0000

    Task1 pycodestyle

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,7 +27,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''Function returns a simple paginated page'''
         assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0, "Both arg must be positive integers!"
+        assert page > 0 and page_size > 0, "Both must be positive integers!"
         first, last = self.index_range(page, page_size)
         return self.dataset()[first:last]
 
