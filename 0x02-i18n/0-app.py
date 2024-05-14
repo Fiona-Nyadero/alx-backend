0b2b14f212e42c229c137624f798201a12b78549
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-15 00:21:15 +0300

    Task 0-corr

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''Index page of the application'''
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
+    '''Main application'''
     app.run(debug=True)
