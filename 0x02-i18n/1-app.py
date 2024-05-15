3c7b759e7e45c19301efcc4a1be8b856f138c2a7
Fiona-Nyadero <fionanyadero@gmail.com>
2024-05-15 05:32:30 +0300

    Task 1 - Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''Module creates an i18n app'''
+from flask import Flask, render_template
+from flask_babel import babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''Babel's Config class'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    '''Index page of the application'''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    '''Main application'''
+    app.run(debug=True)
