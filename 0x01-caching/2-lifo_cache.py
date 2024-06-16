7bffe665d0a86448670f6750167b03ce6c660d26
Fiona-Nyadero <fionanyadero@gmail.com>
2024-06-16 14:48:54 +0300

    Revert "Delete 0x01-caching directory"
    This reverts commit 48eeb178088f89816c9989b8b0c10bc98dbe9978.

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Module creates a LIFO caching algorithm'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''LIFOCache is an inherited class from BaseCaching'''
+
+    def __init__(self):
+        '''Initializes the LIFOCache class'''
+        super().__init__()
+
+    def put(self, key, item):
+        '''Assigns item value to key in dictionary'''
+        if key is not None or item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                all_keys = list(self.cache_data.keys())
+                discarded = all_keys[-1]
+                self.cache_data.pop(discarded)
+                print("DISCARDED: {}".format(discarded))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''Retrieves the value of key'''
+        if key is None:
+            return None
+        elif key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''Module implements a LRU Caching algorithm'''
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''LRUCache is an inherited class from BaseCaching'''
+
+    def __init__(self):
+        '''Initialises the class LRUCache'''
+        super().__init__()
+        self.key_order = []
+
+    def put(self, key, item):
+        '''Assigns item(value) to a key in the dictionary'''
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded = self.key_order.pop(0)
+                self.cache_data.pop(discarded)
+                print("DISCARD: {}".format(discarded))
+            self.cache_data[key] = item
+            self.key_order.append(key)
+
+    def get(self, key):
+        '''Retrieves the key from the dictionary'''
+        if key in self.cache_data:
+            self.key_order.remove(key)
+            self.key_order.append(key)
+            return self.cache_data.get(key)
