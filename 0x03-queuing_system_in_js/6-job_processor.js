efd461f18b71b9617025bd3283c450b692b47922
Fiona-Nyadero <fionanyadero@gmail.com>
2024-07-02 13:31:44 +0000

    Task 7 - Create the Job processor

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -12,11 +12,12 @@ console.log('Queue created successfully');
 
 // sendNotification function implementation
 function sendNotification(phoneNumber, message) {
-	console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
 
-// Process job Queue & handle errors
+// Process job Queue
 queue.process('push_notification_code', (job, done) => {
+	console.log('Processing job:', job.id);
 	const { phoneNumber, message } = job.data;
 	sendNotification(phoneNumber, message);
 	done();
